7a0122106c2bcbbead24b3d8ab5cb2ff752568fb
unknown <nico.fcok@gmail.com>
2023-02-15 23:30:23 -0300

    haciendo pruebas en back con panel de front menu

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const app = express();
-require('dotenv').config()
-const port= process.env.PORT;
+const cors = require('cors');
+const port = 8000;
+// require('dotenv').config()
+// const port= process.env.PORT;
 
+app.use(cors());
+app.use(express.json());
 
-app.get('/obtenerinformacion'),(res,req) =>(
-  res.send('Obtuviste informacion')
-)
+app.get('/obtener-info', (req, res) => {
+  res.send('Obtuviste informacion');
+}); 
+
+app.post('/enviar-info', (req, res) => {
+  console.log(req.body)
+  res.send({
+    status: 200,
+    message: 'Data enviada!'
+  })
+})
+
 app.listen(port, () => {
   console.log(`Estamos trabajando en el siguiente link http://localhost:${port}`)
 })
-
